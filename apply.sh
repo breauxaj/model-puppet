9ff915022b807e9c347971d0560aa5995cd158ac
Adam Breaux <breauxa@email.chop.edu>
2015-05-07 13:13:13 -0400

    remainder of the hiera pieces

--- apply.sh
+++ apply.sh
@@ -5,7 +5,7 @@
 
 LIBRARIAN=/usr/bin/librarian-puppet
 PUPPET=/usr/bin/puppet
-HIERA=/etc/puppet/hiera
+HIERA=/etc/puppet/hiera/hiera.yaml
 
 PARAMS="apply --hiera_config=${HIERA} --modulepath ./modules manifests/site.pp"
 
