72b5a6d7b4f6b84747e1b6f7f7262c41bb8bb543
Adam Breaux <breauxaj@gmail.com>
2015-10-02 10:11:32 -0400

    revised to line up with actual use

--- init.sh
+++ init.sh
@@ -3,22 +3,16 @@
 # init.sh                                                                    #
 ##############################################################################
 
-YUM=/usr/bin/yum
-GEM=/usr/bin/gem
-BUNDLE=/usr/bin/bundle
-LIBRARIAN=/usr/bin/librarian-puppet
-
 if [ ! -f /root/.gemrc ]; then
   cp /etc/puppet/.gemrc /root/.gemrc
 fi
 
 cd /etc/puppet
 
-if [ ! -d .librarian ]; then
-  ${YUM} -y install ruby-devel
-  ${GEM} install bundler --no-ri --no-rdoc
-  ${BUNDLE} install
-  ${LIBRARIAN} install
+if [ ! -d modules ]; then
+  yum -y -q install ruby-devel
+  gem install bundler --no-ri --no-rdoc
+  bundle install
 else
   echo "Already initialized."
 fi
